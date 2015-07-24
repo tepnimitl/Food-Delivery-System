91904db26e115364b0627fddc755544f96b9eb90
Tepnimit <tepnimitl@gmail.com>
2015-07-24 04:40:14 +0000

    Update feature routing be able tp get value from both select and input, and update new display

--- mapdirection.php
+++ mapdirection.php
@@ -3,10 +3,11 @@
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
-<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-    <title>Directions service</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+  <title>Directions service</title>
     <style>
       html, body, #map-canvas {
         height:100%;
@@ -30,8 +31,8 @@
       }
 #overmap {
   position:absolute;
-  top:20px;
-  left:20px;
+  top:0%;
+  left:0%;
   z-index:99;
 }
 /*
@@ -71,7 +72,7 @@ function initialize() {
   directionsDisplay = new google.maps.DirectionsRenderer();
   var chicago = new google.maps.LatLng(38.892721,-77.079252);
   var mapOptions = {
-    zoom:7,
+    zoom:10,
     center: chicago
   };
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
@@ -109,6 +110,35 @@ function calcRouteSelect() {
   });
 }
 
+function calcRoute() {
+  var start1 = document.getElementById('start1').value;
+  var start2 = document.getElementById('start2').value;
+if (start1 !== ''){
+  var start = start1;
+} else if (start2 !== ''){
+  var start = start2;
+} else {
+}
+  var end1 = document.getElementById('end1').value;
+  var end2 = document.getElementById('end2').value;
+if (end1 !== ''){
+  var end = end1;
+} else if (end2 !== ''){
+  var end = end2;
+} else {
+}
+
+  var request = {
+      origin:start,
+      destination:end,
+      travelMode: google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
 
 
 google.maps.event.addDomListener(window, 'load', initialize);
@@ -118,12 +148,12 @@ google.maps.event.addDomListener(window, 'load', initialize);
   <body>
     <div data-role="page" >
       <div data-role="panel" id="overlayPanel" data-display="overlay">
-        <h2>Overlay Panel</h2> 
-        <p>Some text here</p>
+        <div align="center"><h1>Geo Delivery</h1></div>
+        <p>Try it !</p>
 
 <br>
-    <b>Start: </b>
-    <select id="start1" onchange="calcRouteSelect();">
+    <b>Start from: </b>
+    <select id="start1" onchange="document.getElementById('start2').value='';calcRoute();">
       <option value="">Select starting point</option>
       <?php require 'db/connect.php';
       $result=$conn->query("SELECT address,name FROM place"); 
@@ -133,9 +163,12 @@ google.maps.event.addDomListener(window, 'load', initialize);
       <option value="<?php echo $row['address']; ?>"><?php echo $row['name']?></option>
       <?php }} ?>    
     </select>
-
-    <b>End: </b>
-    <select id="end1" onchange="calcRouteSelect()">
+      <!--Text Input-->
+      <input type="text" id='start2' onchange="document.getElementById('start1').value='';calcRoute();" onfocus="value='';" value="or input your starting point here">
+      </input>
+    <br>
+    <b>Destination: </b>
+    <select id="end1" onchange="document.getElementById('end2').value='';calcRoute();">
       <option value="">Select Destination</option>
       <?php require 'db/connect.php';
       $result=$conn->query("SELECT address,name FROM place"); 
@@ -145,21 +178,26 @@ google.maps.event.addDomListener(window, 'load', initialize);
       <option value="<?php echo $row['address']; ?>"><?php echo $row['name']?></option>
       <?php }} ?>    
     </select>
-
-      <input type="text" id='start2' onchange="calcRouteInput()">
-        <option value=""></option>
+      <!--Text Input-->
+      <input type="text" id='end2' onchange="document.getElementById('end1').value='';calcRoute();" onfocus="value='';" value="or input your destination here">
       </input>
-      <input type="text" id='end2' onchange="calcRouteInput()">
-        <option value=""></option>
-      </input>
-      <input type="submit" value="Submit">
-
-      <div align="center">  <a href="#panel" data-rel="close" class="ui-btn ui-btn-inline ui-shadow ui-corner-all ui-btn-a ui-icon-delete ui-btn-icon-left ui-mini">Close panel</a></div>
+      <div align="center">  
+        <table>
+          <tr>
+            <td>
+              <input type="submit" value="Submit">
+            </td>
+            <td>
+              <a href="#panel" data-rel="close" class="ui-btn ui-btn-inline ui-shadow ui-corner-all ui-btn-a ui-icon-delete ui-btn-icon-right">Close</a>
+            </td>
+          </tr>
+        </table>
+      </div>
 
     </div>
   </div>
   <div data-role="panel" id="overmap" class="ui-content">
-    <a href="#overlayPanel" class="ui-btn ui-btn-inline ui-corner-all ui-shadow">Overlay Panel</a>
+    <p><a href="#overlayPanel" class="ui-btn ui-btn-b ui-btn-inline ui-overlay-shadow ui-corner-all ui-shadow"> CLICK HERE <span class="glyphicon glyphicon-th-list"></span></a></p>
   </div>
 
     <div id="map-canvas"></div>
