d5677d684c596e285c168449305ba62754d03ade
Tepnimit <tepnimitl@gmail.com>
2015-07-27 06:08:19 +0000

    Add index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Login Form</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<body id="background">
+  <?php
+    include 'headerbar.php';
+  ?>
+<div id="main">
+<div id="login">
+<h1 align="center" style=color:#FFF>Welcome to Geo Delivery</h1>
+<button type="button" class="btn btn-info btn-block" onclick="location.href='mapDirection.php'" ><h3>Enter to the website</h3></button>
+</div>
+</div>
+
+</body>
+</html>
